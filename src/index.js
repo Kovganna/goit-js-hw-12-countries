da779504974dfd7b0261a4c52a93985e229a9ce5
Ganna <annakovalenko168@gmail.com>
2021-08-23 15:27:33 +0300

    contries
    добавлен шаблон разметки для выведения списка стран от 02-х до 10-ти

--- src/index.js
+++
@@ -1,9 +0,0 @@
-
-import './fetchCountries.js';
-
-
-
-
-
-  
-
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,4 @@
+export default function fetchCountries(name) {
+    return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+    .then(res => res.json());
+  }
